9630dc29386118d6ffc8d6d318e6f7cdc8df4e76
lawalrafiat <61961857+lawalrafiat@users.noreply.github.com>
2020-03-24 23:20:22 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const name= 'Lawal Rafiat';
+
+console.log(name);
+
+const courses = ['HTML', 'CSS', 'JAVASCRIPT', 'UI/UI'];
+
+console.log(courses);
+
+for (let i = 1; i<= 200; i++) {
+    if (i % 2 !== 1)
+    console.log(i);
+} 
